a03ae001faddb9e23d4b7f093950e150dbd63bd9
Canned Seagull <101097344+Canned-Seagull@users.noreply.github.com>
2026-01-20 23:27:13 +0800

    fix: Unreactive game data store

--- src/stores/game_data.ts
+++ src/stores/game_data.ts
@@ -2,15 +2,14 @@ import type { DataSource } from '@cannedseagull/endless-sky-data-parser';
 import { GameData } from '@cannedseagull/endless-sky-data-parser';
 
 import { defineStore } from 'pinia';
-import { shallowRef } from 'vue';
+import { shallowRef, triggerRef } from 'vue';
 
 export const useGameDataStore = defineStore('gameData', () => {
   const gameData = shallowRef(new GameData());
 
   async function loadDataSource(dataSource: DataSource) {
-    const gd = gameData.value;
-    await gd.loadDataSource(dataSource);
-    gameData.value = gd;
+    await gameData.value.loadDataSource(dataSource);
+    triggerRef(gameData);
   }
 
   return { gameData, loadDataSource };
